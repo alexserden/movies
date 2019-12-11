d4417767ca2c33f98de6f1ba6b1298c1cadb3cf5
alexander <alexserden@gmail.com>
2019-12-11 16:55:46 +0200

    Home-work-2

---
+++ src/main/java/com/solvve/movies/domain/Movie.java
@@ -0,0 +1,23 @@
+package com.solvve.movies.domain;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Data
+@Entity
+public class Movie {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+    private String title;
+    private String country;
+    private String genre;
+    private int year;
+    private int duration;
+    private String description;
+}
---
+++ src/test/java/com/solvve/movies/repository/MovieRepositoryTest.java
@@ -0,0 +1,29 @@
+package com.solvve.movies.repository;
+
+import com.solvve.movies.domain.Movie;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Sql(statements = "delete from movie", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+public class MovieRepositoryTest {
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+    @Test
+    public void testSave(){
+        Movie m = new Movie();
+        m = movieRepository.save(m);
+        assertNotNull(m.getId());
+        assertTrue(movieRepository.findById(m.getId()).isPresent());
+    }
+}
