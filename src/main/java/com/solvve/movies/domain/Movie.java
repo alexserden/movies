aedcb8e5612f49176a0d2c4f045077a9396e6baf
Александр <alexserden@gmail.com>
2019-12-15 13:56:50 +0200

    Home-work-3

--- src/main/java/com/solvve/movies/domain/Movie.java
+++ src/main/java/com/solvve/movies/domain/Movie.java
@@ -10,7 +10,6 @@ import java.util.UUID;
 @Data
 @Entity
 public class Movie {
-
     @Id
     @GeneratedValue
     private UUID id;
--- src/test/java/com/solvve/movies/repository/MovieRepositoryTest.java
+++ src/test/java/com/solvve/movies/repository/MovieRepositoryTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -14,11 +15,10 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @Sql(statements = "delete from movie", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+@ActiveProfiles("test")
 public class MovieRepositoryTest {
-
     @Autowired
     private MovieRepository movieRepository;
-
     @Test
     public void testSave(){
         Movie m = new Movie();
